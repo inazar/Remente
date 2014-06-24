5c85f8f3d15fbe47cdbcb2409a0e4179169cc454
Назар <nivanenko@gmail.com>
2014-06-24 20:04:57 +0300

    Adjust base url

--- client/src/common/services/baseurl.js
+++ client/src/common/services/baseurl.js
@@ -2,10 +2,10 @@ angular.module('common').factory('$baseurl', [
   '$window', function($window) {
     var baseUrl;
     baseUrl = 'http://remente.herokuapp.com';
-    if ($window.location.protocol === 'file:') {
+    if ($window.location.protocol.substr(0, 4) === 'file') {
       return baseUrl;
     } else {
-      return location.origin;
+      return $window.location.origin;
     }
   }
 ]);
